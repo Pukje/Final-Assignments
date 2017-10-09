c7902f571a2850d5033ff2db8f5e7ae9bd61c772
Steven <steven.toet@student.hu.nl>
2017-10-09 11:03:09 +0200

    test

--- Final Assignments/Kluizen/Kluizen.py
+++ Final Assignments/Kluizen/Kluizen.py
@@ -1,87 +1,98 @@
-MaxKluizen = 12
-ListofFileLines = list()
-KluisNummerLijst = list()
+maxkluizen = 12
+listofFileLines = list()
+kluisnummerlijst = list()
 
-def readFile():
-    del ListofFileLines[:]
-    with open("C:\Users\steve\PycharmProjects\lessen\Final Assignments\Kluizen\kluizen.txt") as f:
+
+def readfile():
+    del listofFileLines[:]
+    with open('Kluizen.txt') as f:
         for line in f:
-            ListofFileLines.append(line)
+            listofFileLines.append(line)
         f.close()
 
-def AantalKluizenVrij():
-    readFile()
-    return MaxKluizen - len(ListofFileLines)
 
-def NieuweKluis():
-    if AantalKluizenVrij() == 0:
+def aantalkluizenvrij():
+    readfile()
+    return maxkluizen - len(listofFileLines)
+
+
+def nieuwekluis():
+    if aantalkluizenvrij() == 0:
         print('Alle kluizen zitten vol.')
         return False
     else:
-        Code = input('Uw code voor de kluis? (Minimaal 4 tekens.) ')
-        if len(Code) < 4:
-            NieuweKluis()
-        for line in ListofFileLines:
-            Kluisli = line.split(';')
-            KluisNummerLijst.append(int(Kluisli[0]))
-        print(KluisNummerLijst)
-        Kluisnummer = NieuwKluisNummer(1, KluisNummerLijst);
+        code = input('Uw code voor de kluis? (Minimaal 4 tekens.) ')
+        if len(code) < 4:
+            nieuwekluis()
+        for line in listofFileLines:
+            kluisli = line.split(';')
+            kluisnummerlijst.append(int(kluisli[0]))
+        print(kluisnummerlijst)
+        kluisnummer = nieuwkluisnummer(1, kluisnummerlijst);
 
-        file = open('C:\Users\steve\PycharmProjects\lessen\Final Assignments\Kluizen\kluizen.txt', 'w')
-        for line in ListofFileLines:
+        file = open('kluizen.txt', 'w')
+        for line in listofFileLines:
             file.write(line)
-        file.write('\n' + str(Kluisnummer) + ';' + Code)
+        file.write('\n' + str(kluisnummer) + ';' + code)
         file.close()
-        print('Uw kluisnummer is:' + str(Kluisnummer))
-        readFile()
+        print('Uw kluisnummer is:' + str(kluisnummer))
+        readfile()
 
-def Kluis_openen():
-    readFile()
-    Code = input('Uw code voor de kluis?')
-    Number = int(input('Uw Kluisnummer?'))
-    if (str(Number) + ';' + Code) in ListofFileLines:
+
+def kluis_openen():
+    readfile()
+    code = input('Uw code voor de kluis?')
+    nummer = int(input('Uw Kluisnummer?'))
+    if (str(nummer) + ';' + code) in listofFileLines:
         print('Uw kluis is open.')
     else:
         print('De code klopt niet, probeer het nog eens.')
-        Kluis_openen()
+        kluis_openen()
 
-def Kluis_teruggeven():
-    readFile()
-    Code = input('Uw code voor de kluis? ')
-    Nummer = int(input('Uw Kluisnummer? '))
-    if (str(Nummer) + ';' + Code) not in ListofFileLines:
+
+def kluis_teruggeven():
+    readfile()
+    code = input('Uw code voor de kluis? ')
+    nummer = int(input('Uw Kluisnummer? '))
+    if (str(nummer) + ';' + code) not in listofFileLines:
         print('Probeer het niet')
-    elif (str(Nummer) + ';' + Code) in ListofFileLines:
-        file = open("C:\Users\steve\PycharmProjects\lessen\Final Assignments\Kluizen\kluizen.txt", 'w')
-        for line in list:
-            if line == (str(Nummer) + ';' + Code):
+    elif (str(nummer) + ';' + code) in listofFileLines:
+        file = open("kluizen.txt", 'w')
+        for line in listofFileLines:
+            if line == (str(nummer) + ';' + code):
                 continue
             file.write(line)
         file.close()
         print('Kluis is weer beschikbaar')
 
-def NieuwKluisNummer(Nummer, KluisListArg):
-    if Nummer in KluisNummerLijst:
-        Nummer = Nummer + 1
-        return NieuwKluisNummer(Nummer, KluisListArg)
+
+def nieuwkluisnummer(nummer, kluislistarg):
+    if nummer in kluisnummerlijst:
+        nummer = nummer + 1
+        return nieuwkluisnummer(nummer, kluislistarg)
     else:
-        return Nummer
+        return nummer
+
 
 print('1: Hoeveel kluizen zijn nog vrij.?')
 print('2: Nieuwe kluis.')
 print('3: Iets uit de kluis halen.')
 print('4: Kluis teruggeven.')
-MenuChoice = int(input('Wat wilt u doen? '))
 
-if not isinstance(MenuChoice, int):
+
+menuchoice = int(input('Wat wilt u doen? '))
+
+
+if not isinstance(menuchoice, int):
     print('Error terminating program')
     exit()
 
-if MenuChoice == 1:
-    print(AantalKluizenVrij())
-elif MenuChoice == 2:
-    NieuweKluis()
-elif MenuChoice == 3:
-    Kluis_openen()
-elif MenuChoice == 4:
-    Kluis_teruggeven()
+
+if menuchoice == 1:
+    print(aantalkluizenvrij())
+elif menuchoice == 2:
+    nieuwekluis()
+elif menuchoice == 3:
+    kluis_openen()
+elif menuchoice == 4:
+    kluis_teruggeven()
