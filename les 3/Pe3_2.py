6587b48900f1d9f22f1985df00f10ab9533cb04e
Pukje <31697964+Pukje@users.noreply.github.com>
2017-11-10 13:58:43 +0100

    Delete Pe3_2.py

--- les 3/Pe3_2.py
+++
@@ -1,7 +0,0 @@
-Leeftijd =  eval(input("Geef je leeftijd: "))
-ja = True
-nee = False
-Paspoort =  eval(input("Nederlands paspoort: "))
-
-if Leeftijd >= 18 and Paspoort == True:
-    print("Gefeliciteerd, je mag stemmen!")
